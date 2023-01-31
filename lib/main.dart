6f2abf700d2d32166d245ab57df44d82e2428524
akrammostafaa <akrammostafa114@gmail.com>
2023-01-31 17:06:20 +0200

    ins

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'Features/Splash/presention/views/splash_view.dart';
+
 void main() {
   runApp(const BooklyApp());
 }
